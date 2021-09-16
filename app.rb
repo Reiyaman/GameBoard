d371f71587892a9b7923babc5c735d0e21a4f3b0
Reiyaman <r-hoda0124@docomo.ne.jp>
2021-09-16 16:01:58 +0000

    ログイン機能実装

---
+++ app.rb
@@ -0,0 +1,75 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require './models.rb'
+require 'dotenv/load'
+
+enable :sessions
+
+helpers do
+    def current_user
+        User.find_by(id: session[:user])
+    end
+end
+
+before do
+    Dotenv.load
+    Cloudinary.config do |config|
+        config.cloud_name = ENV['CLOUD_NAME']
+        config.api_key = ENV['CLOUDINARY_API_KEY']
+        config.api_secret = ENV['CLOUDINARY_API_SECRET']
+    end
+end
+
+get '/' do
+    erb :index
+end
+
+get '/sign_in' do #ログイン画面に飛ぶ
+    erb :sign_in
+end
+
+get '/sign_up' do #新規登録画面に飛ぶ
+    erb :sign_up
+end
+
+post '/sign_in' do
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+        redirect '/'
+    else
+        redirect '/'
+    end
+end
+
+post '/sign_up' do
+    
+    img_url = '' #ファイルのアップロード
+    if params[:profile]
+        img = params[:profile]
+        tempfile = img[:tempfile]
+        upload = Cloudinary::Uploader.upload(tempfile.path)
+        img_url = upload['url']
+    end
+    
+    user = User.create(
+        name: params[:name],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation],
+        profile: img_url
+        )
+        
+    if user.persisted?
+        session[:user] = user.id
+        redirect '/'
+    else
+        redirect '/'
+    end
+end
+
+get '/sign_out' do #ログアウト
+    session[:user] = nil
+    redirect '/'
+end
+    
