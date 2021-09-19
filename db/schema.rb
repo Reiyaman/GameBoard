0aeabfe92d8e6da9db8540f05069e0d1cc71040c
Reiyaman <r-hoda0124@docomo.ne.jp>
2021-09-19 14:39:26 +0000

    Joinテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_142351) do
+ActiveRecord::Schema.define(version: 2021_09_19_142805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_09_19_142351) do
     t.string "gamename"
     t.integer "model_id"
     t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "joins", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "talkroom_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- models.rb
+++ models.rb
@@ -6,6 +6,9 @@ ActiveRecord::Base.establish_connection
 class User < ActiveRecord::Base
     has_secure_password
     has_many :recruits
+    has_many :chats
+    has_many :joins
+    has_many :join_talkrooms, :through => :joins, :source => :talkroom
 end
 
 class Recruit < ActiveRecord::Base
@@ -29,4 +32,21 @@ end
 class Category < ActiveRecord::Base
     has_many :games
     has_many :recruits
+end
+
+class Talkroom < ActiveRecord::Base
+    belongs_to :recruit
+    has_many :chats
+    has_many :joins
+    has_many :join_users, :through => :joins, :source => :use
+end
+
+class Chat < ActiveRecord::Base
+    belongs_to :talkroom
+    belongs_to :user
+end
+
+class Join < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :talkroom
 end
