3289aad438c08f5c12b30f1f80f20e5b40aa1776
Reiyaman <r-hoda0124@docomo.ne.jp>
2021-10-03 15:52:35 +0000

    チャット欄記入時Enter押しても送信されないように実装

--- public/js/message.js
+++ public/js/message.js
@@ -80,3 +80,13 @@ $('.fa-paper-plane').on('click', function() {
     }
     });
 });
+
+document.getElementById("form_id").onkeypress = (e) => {
+  // form1に入力されたキーを取得
+  const key = e.keyCode || e.charCode || 0;
+  // 13はEnterキーのキーコード
+  if (key == 13) {
+    // アクションを行わない
+    e.preventDefault();
+  }
+}
